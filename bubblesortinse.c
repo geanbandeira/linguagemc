5e9bfc5992831b0c66f1db9ca7404b5789e16a19
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2022-04-06 23:58:32 -0300

    Add files via upload

---
+++ bubblesortinse.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10
+
+void insertion(int *a);
+int main(){
+int i, vet[TAM];
+ 
+	printf("Digite 10 Valores \n");
+	for(i = 0; i < TAM; i++){
+	scanf("%d", &vet[i]);
+}
+
+void insertion(int *a){
+int i, j, tmp;
+  
+for(i = 1; i < TAM; i++){
+tmp = a[i];
+for(j = i-1; j >= 0 && tmp < a[j]; j--){
+a[j+1] = a[j];
+}
+a[j+1] = tmp;
+	}
+}
+
+insertion(vet);
+printf("Valores ordenados\n");
+for(i = 0; i < TAM; i++){
+printf("%d\n", vet[i]);
+}
+
+system("pause");
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
