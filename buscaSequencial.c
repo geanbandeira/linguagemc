d18909d8a3dd1a11d1fe367aaa737d73fa0bdbc7
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2022-04-09 15:21:04 -0300

    Update and rename exemploodernacao.c to buscaSequencial.c

---
+++ buscaSequencial.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <locale.h>
+#define TAM 10
+
+int main(){
+	int vet[TAM];
+	int buscar, i;
+	int n=10;
+	int resp[n];
+	int p=0;
+	
+printf("Entre com os todos os números que deseja\n", setlocale(LC_ALL,""));
+for(i=0;i<TAM;i++){
+	scanf("%d", &vet[i]);
+}
+printf("Esses foram os números escolhidos\n", setlocale(LC_ALL,""));
+for(i=0;i<TAM;i++){
+	printf( "%d\n", vet[i]);
+}
+	
+	printf("Entre com o número que voce deseja encontrar\n", setlocale(LC_ALL,""));
+	scanf("%d", &buscar);
+	for(i=0;i<=n-1;i++){
+		if(vet[i] == buscar){
+			resp[p]=i;
+			p=p+1;
+		}
+	}
+	if(p>0){
+		for(i=0;i<=p-1;i++){
+			printf("Número %d encontrado, ele está na posição %d", buscar, resp[i], setlocale(LC_ALL,""));
+		}
+	}else{
+		printf("Número não encontrado", setlocale(LC_ALL,""));
+	}
+
+return 0;
+}
